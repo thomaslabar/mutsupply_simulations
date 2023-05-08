6f031419977d17d1de57fb5ecf6041f292247719
Thomas LaBar <thomasrlabar@gmail.com>
2023-05-08 12:02:49 -0400

    Changes to printing during sims

--- sim.jl
+++ sim.jl
@@ -184,31 +184,31 @@ function run_sim()
     #Evolve large populations to restore fitness
     for i in 1:params.replicates
         large_populations[i] = evolve(large_populations[i],"Fitness",1.0)
-        print_abundant_clades(large_populations[i],1)
     end
     save_populations("test.csv",large_populations,params,false)
+    println("Large populations restored and saved")
 
     #Evolve small populations for same number of generations as mean large population
     gen_target = ceil(get_avg_generation(large_populations))
     for i in 1:params.replicates
         small_populations[i] = evolve(small_populations[i],"Generation",gen_target)
-        print_abundant_clades(small_populations[i],1)
     end
     save_populations("test.csv",small_populations,params,true)
+    println("Small populations evolved for same avg. generation as large populations and saved")
 
     #Evolve small populations to restore fitness
     for i in 1:params.replicates
         small_populations[i] = evolve(small_populations[i],"Fitness",1.0)
-        print_abundant_clades(small_populations[i],1)
     end
     save_populations("test.csv",small_populations,params,true)
+    println("Small populations restored and saved")
 
     #Evolve small populations to same total mutation supply (N*mu*g) as large populations
     for i in 1:params.replicates
         small_populations[i] = evolve(small_populations[i],"Generation",gen_target*(params.largepopulationsize/params.smallpopulationsize))
-        print_abundant_clades(small_populations[i],1)
     end
     save_populations("test.csv",small_populations,params,true)
+    println("Small populations evolved for same total mutation supply as large populations and saved")
 end
 
 run_sim()
