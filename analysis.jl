6b37b0c368bc90c81e2fd3ba79a8e538b8d1bfd6
Thomas LaBar <thomasrlabar@gmail.com>
2023-05-17 09:31:52 -0400

    Fixed mutation stats to handle empty arrays

--- analysis.jl
+++ analysis.jl
@@ -12,8 +12,13 @@ function get_mutation_stats(df::DataFrame)
 
     #The array of mutational effects is read as a string by Julia. The below line handles this by 
     #removing the string quotations, spliting up the data by commas, then parses to Float64. This results
-    # in an array of arrays. NOTE: This currently can't handle empty arrays of Float64[] text.
-    mutation_data = [parse.(Float64, split(chop(mutations[i]; head=1, tail=1), ',')) for i in eachindex(mutations)]
+    # in an array of arrays.
+    
+    mutation_strings = [chop(mutations[i]; head=1, tail=1) for i in eachindex(mutations)]
+    mutation_strings = [split(mutation_strings[i], ',') for i in eachindex(mutation_strings)]
+    mutation_strings = [i for i in mutation_strings if i!= ["loat64["]] #eliminates empty arrays (for pops with no fixed mutations)
+
+    mutation_data = [parse.(Float64, mutation_strings[i]) for i in eachindex(mutation_strings)]
 
     #flattens the above array of arrays to one array
     mutation_data = reduce(vcat,mutation_data)
@@ -30,6 +35,9 @@ function run_analysis(csv_file::String)
 
     small_pop_data = filter(:generation => n -> n == 569, df)
     get_mutation_stats(small_pop_data)
+
+    small_totalmutsupply_pop_data = filter(:generation => n -> n == 56900, df)
+    get_mutation_stats(small_totalmutsupply_pop_data)
     
 end
 
